0f2e30ba3624b31c3555d5b66260ba075e89dca6
cooperzilla <cooperye09@gmail.com>
2024-06-14 20:12:50 -0500

    withdraw size and command updates

--- src/main/java/me/cooperzilla/resizesmp/Commands/Debug.java
+++ src/main/java/me/cooperzilla/resizesmp/Commands/Debug.java
@@ -41,24 +41,52 @@ public class Debug implements CommandExecutor {
                 }
             }
             case "give" -> {
+                int amount;
+                if (args[1] == null) {
+                    amount = 1;
+                } else {
+                    amount = Integer.parseInt(args[1]);
+                }
+
                 switch (args[1]) {
                     case "revive" -> {
-                        ItemStack totem = new ItemStack(Material.TOTEM_OF_UNDYING);
-                        ItemMeta meta = totem.getItemMeta();
+                        for (int i = 0; i < amount; i++) {
+                            ItemStack totem = new ItemStack(Material.TOTEM_OF_UNDYING);
+                            ItemMeta meta = totem.getItemMeta();
 
-                        meta.setDisplayName("Revival Totem");
-                        meta.setLore(Collections.singletonList("Run /revive <name>"));
+                            meta.setDisplayName("Revival Totem");
+                            meta.setLore(Collections.singletonList("Run /revive <name>"));
 
-                        var container = meta.getPersistentDataContainer();
-                        container.set(
-                                new NamespacedKey(ResizeSMP.getPl().getName(),"revive"),
-                                PersistentDataType.STRING,
-                                "Some Value"
-                        );
+                            var container = meta.getPersistentDataContainer();
+                            container.set(
+                                    new NamespacedKey(ResizeSMP.getPl().getName(),"revive"),
+                                    PersistentDataType.STRING,
+                                    "Some Value"
+                            );
 
-                        totem.setItemMeta(meta);
+                            totem.setItemMeta(meta);
 
-                        p.getInventory().addItem(totem);
+                            p.getInventory().addItem(totem);
+                        }
+                    }
+                    case "size" -> {
+                        for (int i = 0; i < amount; i++) {
+                            ItemStack item = new ItemStack(Material.RED_DYE);
+                            ItemMeta meta = item.getItemMeta();
+
+                            meta.setDisplayName("Congealed Size");
+
+                            var container = meta.getPersistentDataContainer();
+                            container.set(
+                                    new NamespacedKey(ResizeSMP.getPl().getName(), "congealed_size"),
+                                    PersistentDataType.STRING,
+                                    "Some Value"
+                            );
+
+                            item.setItemMeta(meta);
+
+                            p.getInventory().addItem(item);
+                        }
                     }
                 }
             }
